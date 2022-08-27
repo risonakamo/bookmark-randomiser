82905d886c0cb9f9394af13e065bbd8ff3d0951a
risonakamo <hmykanae@gmail.com>
2022-08-27 15:53:45 -0400

    laying out bookmark generate page

--- web/pages/bookmark-generate/bookmark-generate-index.tsx
+++ web/pages/bookmark-generate/bookmark-generate-index.tsx
@@ -1,10 +1,34 @@
 import {createRoot} from "react-dom/client";
 
+import FolderBackButton from "components/folder-back-button/folder-back-button";
+import StaticToastBar from "components/static-toast-bar/static-toast-bar";
+import AmountSelector from "components/amount-selector/amount-selector";
+import GenerateButton from "components/generate-button/generate-button";
+import FatButton from "components/fat-button/fat-button";
+
 import "./bookmark-generate-index.less";
 
 export default function BookmarkGenerateIndex():JSX.Element
 {
+  const testpath:string[]=[
+    "folder1",
+    "asdads",
+    "asdadasdasda"
+  ];
+
   return <>
+    <div className="control-header">
+      <FolderBackButton/>
+      <StaticToastBar path={testpath}/>
+      <AmountSelector/>
+    </div>
+    <div className="control-buttons">
+      <GenerateButton itemCount={52}/>
+      <FatButton text="OPEN"/>
+    </div>
+    <div className="items-zone">
+
+    </div>
   </>;
 }
 
